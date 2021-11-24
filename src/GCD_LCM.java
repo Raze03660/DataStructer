5d5e497044e2b36f97f19ed5ebd5652f8b1b06b5
Raze <eri521414@gmail.com>
2021-11-24 16:40:04 +0800

    10 completed

--- src/GCD_LCM.java
+++ src/GCD_LCM.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
-public class GCD {
+public class GCD_LCM {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        if (gcd(a, b) == 1) {
-            System.out.println("兩數互值");
-        } else {
-            System.out.println(gcd(a, b));
-        }
+        System.out.printf("最大公因數:%d,最小公倍數:%d",gcd(a,b),a*b/gcd(a,b));
     }
 
     public static int gcd(int a, int b) {
-        if (b == 0)
+        if (b == 0) {
             return a;
-        return gcd(b, a % b);
+        } else {
+            return gcd(b, a % b);
+        }
     }
 }
